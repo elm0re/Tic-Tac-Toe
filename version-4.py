2dd3a59e044ba3b021041352ce5a1946e7da7bd1
elm0re <143751586+elm0re@users.noreply.github.com>
2026-01-14 11:51:18 -0500

    Create version-4.py

---
+++ version-4.py
@@ -0,0 +1,31 @@
+player_symbol = "X"
+cpu_symbol = "O"
+
+
+def create_board():
+  return [[1,2,3], [4,5,6],[7,8,9]]
+  
+def display_board(board):
+    for row in board:
+        print(row)
+
+def get_player_choice():
+  choice = int(input("Enter an available number: "))
+  return choice
+  
+def place_move(board, choice, player_symbol):
+  for i in range(3):
+    for j in range(3):
+      if board[i][j] == choice:
+        board[i][j] = player_symbol
+  return board
+
+
+
+
+board = create_board()
+
+while True:
+  display_board(board)
+  choice = get_player_choice()
+  board = place_move(board, choice, player_symbol)
